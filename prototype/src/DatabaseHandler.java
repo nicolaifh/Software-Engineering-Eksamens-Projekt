411d21fe826b8eee0da2020da2fa0fd0382e49c0
Android47 <nicolaifh05@gmail.com>
2026-03-23 19:41:24 +0100

    refactoring file structure

--- prototype/src/DatabaseHandler.java
+++ prototype/src/DatabaseHandler.java
@@ -1,3 +1,18 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
 public class DatabaseHandler {
+    String url = "jdbc:mysql://localhost:3306/SoftwareExam";
+    static {
+    try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+
+
 
 }
