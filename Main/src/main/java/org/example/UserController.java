304a9a4d4c34257bbd9fdd96e283d1d17e1ea155
BENkovitch <benjaminchambers04@gmail.com>
2026-05-04 12:23:07 +0200

    Implementerer fileReader

--- Main/src/main/java/org/example/UserController.java
+++ Main/src/main/java/org/example/UserController.java
@@ -1,9 +1,15 @@
 package org.example;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.List;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+
 
 public class UserController {
     ArrayList<User> users = new ArrayList<User>();
@@ -29,4 +35,29 @@ public class UserController {
     public void setUsers(ArrayList<User> users) {
         this.users = users;
     }
-}
+
+    public ArrayList<User> importUsersFromFile(File file){
+        ArrayList<User> users = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                line = line.trim();
+
+                if (!line.matches("[a-zA-Z]{4}")) {
+                    continue;
+                }
+            }
+
+            users.add(new User(line));
+            }
+
+        catch (IOException e) {
+            throw new RuntimeException(e);
+                }
+
+        return users;
+        }
+    }
